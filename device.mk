981662aa8012bf2d755aa2966a208f326f7bb1a3
bengris32 <bengris32@protonmail.ch>
2026-05-28 10:20:41 +0800

    KJ5: Switch to AOSP audio service & Import missing blobs
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>

--- device.mk
+++ device.mk
@@ -47,11 +47,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.0-impl \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.audio.service.mediatek
+    android.hardware.audio.service \
+    android.hardware.bluetooth.audio-impl
 
 PRODUCT_PACKAGES += \
-    audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.1-impl
+    audio.bluetooth.default
 
 PRODUCT_PACKAGES += \
     libaudiofoundation.vendor \
