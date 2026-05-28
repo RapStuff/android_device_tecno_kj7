9efbdc290597514327b8bd40c9c66e1b676b11c9
sabrina010209 <mcpe11640@gmail.com>
2026-05-28 11:28:32 +0800

    KJ5: libinit: drop unneeded things
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>

--- libinit/init_KJ5.cpp
+++ libinit/init_KJ5.cpp
@@ -5,13 +5,8 @@
  */
 
 #include <libinit_utils.h>
-#include <libinit_dalvik_heap.h>
-#include <unistd.h>
 
 #include "vendor_init.h"
 
 void vendor_load_properties() {
-    if (access("/system/bin/recovery", F_OK) != 0) {
-        set_dalvik_heap();
-    }
 }
