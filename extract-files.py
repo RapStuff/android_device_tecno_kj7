b68147cf60267531bd5a9e3125a18d52817192a5
アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>
2026-05-28 20:24:18 +0800

    proprietary-files: add trustonic tee chain; fix power blob ndk_platform
    - Add vendor/lib64/vendor.trustonic.tee@{1.0,1.1}.so as soong modules
      required by vendor.trustonic.tee@1.1-service (no lib32 in dump).
    - Extend power ndk_platform -> ndk fixup to cover
      android.hardware.power-service-mediatek.so in addition to
      mtkpower@1.0-service.
    
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file.@protonmail.com>
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>

--- extract-files.py
+++ extract-files.py
@@ -58,7 +58,10 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
     'vendor/bin/hw/mtkfusionrild': blob_fixup()
         .add_needed('libutils-v32.so'),
-    'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service': blob_fixup()
+    (
+        'vendor/bin/hw/vendor.mediatek.hardware.mtkpower@1.0-service',
+        'vendor/lib64/android.hardware.power-service-mediatek.so',
+    ): blob_fixup()
         .replace_needed('android.hardware.power-V2-ndk_platform.so', 'android.hardware.power-V2-ndk.so'),
     (
         'vendor/bin/mnld',
