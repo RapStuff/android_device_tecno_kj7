e07b244dc83ab8a05c3be9ce9d722a243f091d83
bengris32 <bengris32@protonmail.ch>
2026-05-28 12:10:27 +0800

    KJ5: Patchelf C2 service to link against libavservices_minijail
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file.@protonmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -75,7 +75,8 @@ function blob_fixup {
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
         vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
-            "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "$2"
+            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "$2"
+            "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
             ;;
         system_ext/lib64/libsource.so)
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
