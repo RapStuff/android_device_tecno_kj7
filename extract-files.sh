6985199728abbf7a136dfcebe583c601793c95da
claxten10 <claxten10@gmail.com>
2026-05-28 12:08:30 +0800

    KJ5: Migrate to 64-bit C2 Media Stack
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file.@protonmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -74,7 +74,7 @@ function blob_fixup {
         vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
             "$PATCHELF" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "$2"
             ;;
-        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek)
+        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
             "$PATCHELF" --add-needed "libstagefright_foundation-v33.so" "$2"
             ;;
         system_ext/lib64/libsource.so)
