d92edcac9c5e1a91f3313a8123950182255d0642
アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>
2026-05-29 09:18:51 +0800

    lineage_KJ5: add Axion build flags and sepolicy genfscon entries
    - TARGET_DISABLE_EPPE := true (required by Axion)
    - Add AXION_CAMERA_REAR_INFO, AXION_CAMERA_FRONT_INFO, AXION_PROCESSOR
    - TARGET_NEEDS_DOZE_FIX := true (MTK sensor/doze quirks)
    - GPU_FREQS_PATH / GPU_MIN_FREQ_PATH for 13040000.mali
    - genfscon entries for Axion perf tuning (proc_dirty, proc_sched,
      proc_drop_caches) and GPU boost sysfs nodes
    
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file.@protonmail.com>
    Signed-off-by: アンドレイ <xtensei.is.not.in.the.sudoers.file@protonmail.com>

--- lineage_KJ5.mk
+++ lineage_KJ5.mk
@@ -11,7 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/tecno/KJ5/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some common AxionOS stuff.
+TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_KJ5
@@ -28,3 +29,13 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildFingerprint=TECNO/KJ5-OP/TECNO-KJ5:12/SP1A.210812.016/250115V1871:user/release-keys
 
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
+
+# Axion device info
+AXION_CAMERA_REAR_INFO := 50,8
+AXION_CAMERA_FRONT_INFO := 8
+AXION_PROCESSOR := MediaTek_Helio_G85
+
+# Axion flags
+TARGET_NEEDS_DOZE_FIX := true
+GPU_FREQS_PATH := /sys/devices/platform/13040000.mali/available_frequencies
+GPU_MIN_FREQ_PATH := /sys/devices/platform/13040000.mali/hint_min_freq
