580885536536f0bb0a952e546cb874f88121633e
Rapli <raffikun1203@gmail.com>
2026-08-29 03:15:45 +0700

    kj7: vintf: Merge NFC manifest into main manifest to fix HAL conflicts

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -237,7 +237,6 @@ WIFI_HAL_INTERFACE_COMBINATIONS += ,{{{STA}, 1}, {{NAN}, 1}}
 # HIDL / VINTF
 DEVICE_MANIFEST_FILE += $(CONFIGS_PATH)/vintf/manifest.xml
 ODM_MANIFEST_SKUS += nfc
-ODM_MANIFEST_NFC_FILES := $(CONFIGS_PATH)/vintf/manifest_nfc.xml
 DEVICE_MATRIX_FILE += $(CONFIGS_PATH)/vintf/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(CONFIGS_PATH)/vintf/device_framework_compatibility_matrix.xml \
