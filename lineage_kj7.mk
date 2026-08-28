3b3adcf9e3695daefa758807151c5796ad6cba31
Rapli <raffikun1203@gmail.com>
2026-08-29 00:16:02 +0700

    kj7: Remove AxionOS-specific flags

--- lineage_kj7.mk
+++ lineage_kj7.mk
@@ -11,8 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/tecno/kj7/device.mk)
 
-# Inherit some common AxionOS stuff.
-TARGET_DISABLE_EPPE := true
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_kj7
@@ -30,12 +28,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
 
-# Axion device info
-AXION_CAMERA_REAR_INFO := 50,8
-AXION_CAMERA_FRONT_INFO := 8
-AXION_PROCESSOR := MediaTek_Helio_G85
-
-# Axion flags
-TARGET_NEEDS_DOZE_FIX := true
-GPU_FREQS_PATH := /sys/devices/platform/13040000.mali/available_frequencies
-GPU_MIN_FREQ_PATH := /sys/devices/platform/13040000.mali/hint_min_freq
